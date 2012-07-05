fdeec45a09b8cb6b2c39b6ea86f60d1a9cfb0bb6
Shivasubramanian A <shivasa@thoughtworks.com>
2012-07-05 23:26:25 +0530

    Started using Maven to build the project.

--- src/main/java/presentation/windows/DefaultController.java
+++ src/main/java/presentation/windows/DefaultController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.classic.Session;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
