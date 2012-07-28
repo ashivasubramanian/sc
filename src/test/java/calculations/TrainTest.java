9d4de1812352631aa19ec8610708cbdde2658d4d
Shivasubramanian A <shivasa@thoughtworks.com>
2012-07-28 12:26:50 +0530

    Testing file renames on Mac.

--- src/test/java/calculations/TrainTest.java
+++ src/test/java/calculations/TrainTest.java
@@ -11,7 +11,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-public class TrainTest {
+public class traintest {
 	
 	@Test
 	public void initializingTrainWithLegalValuesShouldPass() {
