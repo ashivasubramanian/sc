383da2edbe6df9081a970d1c225b767d659bd227
Shivasubramanian A <ashivasubramanian@gmail.com>
2018-02-06 21:47:31 +0530

    Formatting changes

--- src/test/java/calculations/data_access/DataAccessTest.java
+++ src/test/java/calculations/data_access/DataAccessTest.java
@@ -16,59 +16,59 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class DataAccessTest {
-    
-	@Test
-	public void shouldCreateMissingUsersFile() {
-		String filePath = String.join(java.io.File.separator, "src", "test", "resources", "Users.xml");
-		File file = new File(filePath);
-		if (file.exists()) {
-			file.delete();
-		}
-		assertFalse(file.exists());
 
-		DataAccess dataAccess = DataAccess.getInstance();
-		dataAccess.createMissingUsersFile(filePath);
-		assertTrue(file.exists());
+    @Test
+    public void shouldCreateMissingUsersFile() {
+	String filePath = String.join(java.io.File.separator, "src", "test", "resources", "Users.xml");
+	File file = new File(filePath);
+	if (file.exists()) {
+	    file.delete();
+	}
+	assertFalse(file.exists());
+
+	DataAccess dataAccess = DataAccess.getInstance();
+	dataAccess.createMissingUsersFile(filePath);
+	assertTrue(file.exists());
+    }
+
+    @Test
+    public void shouldEnsureThatStandardFolderIsCreatedIfNotExisting() {
+	String standardDirectoryPath = String.join(java.io.File.separator, "src", "test", "resources");
+	String usersXMLPath = String.join(java.io.File.separator, "src", "test", "resources", "Users.xml");
+	File standardDirectoryFile = new File(standardDirectoryPath);
+	File usersXMLFile = new File(usersXMLPath);
+
+	if (standardDirectoryFile.exists()) {
+	    if (usersXMLFile.exists()) {
+		usersXMLFile.delete();
+	    }
+	    standardDirectoryFile.delete();
 	}
 
-	@Test
-	public void shouldEnsureThatStandardFolderIsCreatedIfNotExisting() {
-		String standardDirectoryPath = String.join(java.io.File.separator, "src", "test", "resources");
-		String usersXMLPath = String.join(java.io.File.separator, "src", "test", "resources", "Users.xml");
-		File standardDirectoryFile = new File(standardDirectoryPath);
-		File usersXMLFile = new File(usersXMLPath);
+	DataAccess dataAccess = DataAccess.getInstance();
+	dataAccess.createMissingUsersFile(usersXMLPath);
+	assertTrue(standardDirectoryFile.exists());
+    }
 
-		if (standardDirectoryFile.exists()) {
-		    if (usersXMLFile.exists()) {
-			usersXMLFile.delete();			
-		    }
-		    standardDirectoryFile.delete();
-		}
-
-		DataAccess dataAccess = DataAccess.getInstance();
-		dataAccess.createMissingUsersFile(usersXMLPath);
-		assertTrue(standardDirectoryFile.exists());
+    @Test
+    public void shouldEnsureThatNewlyCreatedUsersXMLHasEmptyContent() {
+	String filePath = String.join(java.io.File.separator, "src", "test", "resources", "Users.xml");
+	File file = new File(filePath);
+	if (file.exists()) {
+	    file.delete();
 	}
 
-	@Test
-	public void shouldEnsureThatNewlyCreatedUsersXMLHasEmptyContent() {
-		String filePath = String.join(java.io.File.separator, "src", "test", "resources", "Users.xml");
-		File file = new File(filePath);
-		if (file.exists()) {
-			file.delete();
-		}
+	DataAccess dataAccess = DataAccess.getInstance();
+	dataAccess.createMissingUsersFile(filePath);
 
-		DataAccess dataAccess = DataAccess.getInstance();
-		dataAccess.createMissingUsersFile(filePath);
-
-		try {
-		    Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
-		    assertEquals(1, document.getElementsByTagName("users").getLength());
-		    assertEquals(0, document.getElementsByTagName("user").getLength());
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-		    e.printStackTrace();
-		    fail();
-		}
+	try {
+	    Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+	    assertEquals(1, document.getElementsByTagName("users").getLength());
+	    assertEquals(0, document.getElementsByTagName("user").getLength());
+	} catch (ParserConfigurationException | SAXException | IOException e) {
+	    e.printStackTrace();
+	    fail();
 	}
+    }
 
 }
