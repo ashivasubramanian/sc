25479604536c924b2fca379b7ce009bc80de74c9
Shivasubramanian A <ashivasubramanian@gmail.com>
2018-04-24 00:01:25 +0530

    Formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,25 @@ import presentation.windows.LoginInterface;
 /**
  * The Main class is the starting point of the application.
  */
-public class Main
-{
-	/**
-	 * The main method for the game.
-	 *
-	 * @param args An array of <code>String</code>s that can be 
-	 * passed as arguments. However, nothing is done with these
-	 * arguments. 
-	 */
-	public static void main( String args[])
-	{
-		try {
-			UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			
-		}
-		try {
-		    LoginInterface objLogin = new LoginInterface();
-		    SwingUtilities.invokeAndWait(objLogin);
-		} catch (InvocationTargetException | InterruptedException e) {
-		    e.printStackTrace();
-		}
+public class Main {
+    /**
+     * The main method for the game.
+     *
+     * @param args
+     *            An array of <code>String</code>s that can be passed as arguments.
+     *            However, nothing is done with these arguments.
+     */
+    public static void main(String args[]) {
+	try {
+	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	} catch (Exception e) {
+
 	}
+	try {
+	    LoginInterface objLogin = new LoginInterface();
+	    SwingUtilities.invokeAndWait(objLogin);
+	} catch (InvocationTargetException | InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
 }
