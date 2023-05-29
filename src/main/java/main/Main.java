a274e9096563ed3c61cb9b73867cae60a90fb9b9
Shivasubramanian A <ashivasubramanian@gmail.com>
2023-05-29 21:12:29 +0530

    #22 Generate standalone artifact using jlink tool

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.SwingUtilities;
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module section_controller {
+    requires java.base;
+    requires java.xml;
+    requires java.desktop;
+}
